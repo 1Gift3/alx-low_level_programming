8e9e3f547c59fb8b644e059f7ee3bf5b37d6578f
1Gift3 <joshuaramaoka@gmail.com>
2023-02-17 01:34:17 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ printf("Size of a char: %lu byte(s)\n", sizeof(a));
 printf("Size of an int: %lu byte(s)\n", sizeof(b));
 printf("size of a long int: %lu byte(s)\n", sizeof(c));
 printf("size of a long long int: %lu byte(s)\n", sizeof(d));
-printf("size of a float: %lu byte(s)", sizeof(f));
+printf("size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
 
