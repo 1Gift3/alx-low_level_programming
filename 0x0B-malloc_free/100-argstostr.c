6cb0392232e9a3db9862c20260a5f2fad34d8bd4
1Gift3 <joshuaramaoka@gmail.com>
2023-06-13 02:20:52 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,34 +6,36 @@
  * @av: double pointer of array
  * Return: nothing
  */
+
 char *argstostr(int ac, char **av)
 {
-
-	int a, n, r = 0, l = 0;
+	int i, n, r = 0, l = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (a = 0; a < ac; a++)
+
+	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
 			l++;
 	}
 	l += ac;
+
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
-	for (a = 0; a < ac; a++)
+	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[a][n]; n++)
-		{
-			str[r] = av[a][n];
-			r++;
-		}
-		if (str[r] == '\0')
-		{
-			str[r++] = '\n';
-		}
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
 	}
 	return (str);
 }
